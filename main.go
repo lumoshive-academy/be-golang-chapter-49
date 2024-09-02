1c0bd02bd001a7e60f0e4c82bc5cfd1448eb310a
lumoshive-academy <lumoshive.academy@gmail.com>
2024-09-02 20:57:12 +0700

    change config

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"be-golang-chapter-49/config"
 	"be-golang-chapter-49/handler"
 	"log"
 	"net/http"
 )
 
 func main() {
+	config.LoadConfig()
+
 	http.HandleFunc("/send-otp", handler.OtpHandler)
 
 	log.Println("Server is running on http://localhost:8080")
